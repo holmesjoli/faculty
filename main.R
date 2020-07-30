98dce5c9f0f0623fb824a9dedaeae837091aaa78
Joli Holmes <holmesjoli@gmail.com>
2020-07-30 12:09:35 -0500

    added step to write out dat

--- main.R
+++ main.R
@@ -9,14 +9,37 @@ um <- faculty.um()
 l <- list(uw, um) %>% 
   purrr::transpose()
 
-id <- do.call(rbind, l[["id"]])
+id <- do.call(rbind, l[["id"]]) %>% 
+  dplyr::mutate(phd_adv = gsub("Potential PhD Faculty Advisor: ", "", phd_adv),
+                phd_adv = ifelse(phd_adv == "", NA, phd_adv),
+                emeritus = grepl("emeritus", tolower(title)),
+                research_fellow = grepl("research fellow|fellow", tolower(title))) %>% 
+  dplyr::filter(phd_adv %in% c("Yes", NA)) %>% 
+  dplyr::filter(!(title %in% c("Ph.D. Candidate", "Ph.D. Student"))) %>% 
+  dplyr::filter(emeritus == FALSE & research_fellow == FALSE)
 
-specializations <- do.call(rbind, l[["specializations"]])
+potential_adv <- id$email
 
-research_area <- do.call(rbind, l[["research_area"]])
+write.csv(id, "./data/id.csv", row.names = FALSE)
 
-bio <- do.call(rbind, l[["bio"]])
+specializations <- do.call(rbind, l[["specializations"]]) %>% 
+  dplyr::filter(email %in% potential_adv)
+write.csv(specializations, "./data/specializations.csv", row.names = FALSE)
 
-edu <- do.call(rbind, l[["edu"]])
+research_area <- do.call(rbind, l[["research_area"]]) %>% 
+  parse.research_area() %>% 
+  dplyr::filter(email %in% potential_adv)
+write.csv(research_area, "./data/research_area.csv", row.names = FALSE)
 
-pub <- do.call(rbind, l[["pub"]])
+bio <- do.call(rbind, l[["bio"]]) %>% 
+  dplyr::filter(email %in% potential_adv)
+write.csv(bio, "./data/biography.csv", row.names = FALSE)
+
+edu <- do.call(rbind, l[["edu"]]) %>% 
+  dplyr::filter(email %in% potential_adv) %>% 
+  parse.edu()
+write.csv(edu, "./data/education.csv", row.names = FALSE)
+
+pub <- do.call(rbind, l[["pub"]]) %>% 
+  dplyr::filter(email %in% potential_adv)
+write.csv(pub, "./data/publications.csv", row.names = FALSE)
