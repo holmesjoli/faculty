7d10971d84cadd156adc1f7dbab390354d7ced02
Joli Holmes <holmesjoli@gmail.com>
2020-07-30 10:41:48 -0500

    added additional school

--- main.R
+++ main.R
@@ -1,3 +1,22 @@
 library(magrittr)
 
+sapply(list.files("R", full.names = TRUE, recursive = TRUE), source, .GlobalEnv)
+
 uw <- faculty.uw()
+
+um <- faculty.um()
+
+l <- list(uw, um) %>% 
+  purrr::transpose()
+
+id <- do.call(rbind, l[["id"]])
+
+specializations <- do.call(rbind, l[["specializations"]])
+
+research_area <- do.call(rbind, l[["research_area"]])
+
+bio <- do.call(rbind, l[["bio"]])
+
+edu <- do.call(rbind, l[["edu"]])
+
+pub <- do.call(rbind, l[["pub"]])
