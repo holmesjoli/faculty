e95247860d8ce93e8b41bfe4b4cfc79a96c84f5a
Joli Holmes <holmesjoli@gmail.com>
2020-07-31 12:05:26 -0500

    scraped university of illinois

--- R/scrape_faculty.R
+++ R/scrape_faculty.R
@@ -8,12 +8,13 @@ parse.research_area <- function(df) {
                   sts = grepl("science, technology, and society|science, technology & society|sts", tolower(research_area)),
                   archive = grepl("archive", tolower(research_area)),
                   hci = grepl("human-computer interaction|human computer interaction|hci", tolower(research_area)),
-                  visualization = grepl("visualization", tolower(research_area)))
+                  visualization = grepl("visualization", tolower(research_area)),
+                  ethics = grepl("ethics", tolower(research_area)))
 }
 
 #' @title Parse Education
 parse.edu <- function(df) {
-  
+
   df <- df %>%
     dplyr::mutate(phd = grepl("PhD|Ph D|Ph.D.", edu),
                   ms = grepl("MS|M.S.|Master|master|ScM|MEng|MSC", edu),
@@ -38,6 +39,25 @@ parse.edu <- function(df) {
   return(df)
 }
 
+#' @title Parse ID
+parse.id <- function(df) {
+  
+  df %>% 
+    dplyr::mutate(
+      emeritus = grepl("emeritus", tolower(title)),
+      research_fellow = grepl("research fellow|fellow", tolower(title)),
+      adjunct = grepl("adjunct", tolower(title)),
+      affiliate = grepl("affiliate", tolower(title)),
+      assistant = grepl("assistant", tolower(title)),
+      associate = grepl("associate", tolower(title)),
+      lecturer = grepl("lecturer", tolower(title)),
+      phd = grepl("ph.d. candidate|ph.d. student", tolower(title)),
+      phd_adv = gsub("Potential PhD Faculty Advisor: ", "", phd_adv),
+      phd_adv = ifelse(phd_adv == "", NA, phd_adv)
+      )
+
+}
+
 #' @param url string. The url
 #' @param selector string. The selctor to use to scrape the page.
 scrape_links <- function(url, selector) {
@@ -52,11 +72,18 @@ scrape_links <- function(url, selector) {
 scrape_text <- function(page, selector) {
 
   if(!is.null(selector)) {
-    return(page %>% 
+    
+    txt <- page %>% 
       rvest::html_nodes(selector) %>% 
       rvest::html_text() %>% 
       stringr::str_trim("both")
-    )
+    
+    if(length(txt) == 0) {
+      return(NA)
+    } else {
+      return(txt)
+    }
+
   } else {
     return(NA)
   }
@@ -96,9 +123,11 @@ collect_info <- function(link, ischool,
                          bio_selector = NULL,
                          edu_selector = NULL,
                          pub_selector = NULL,
-                         phd_selector = NULL) {
+                         phd_selector = NULL,
+                         delay = 2) {
 
   page <- xml2::read_html(link)
+  print(link)
 
   name <- scrape_text(page = page, selector = name_selector)
   assertthat::assert_that(length(name) == 1 | is.null(name))
@@ -111,6 +140,13 @@ collect_info <- function(link, ischool,
   
   phone <- scrape_text(page = page, selector = phone_selector)
   assertthat::assert_that(length(phone == 1) | is.null(phone))
+
+  if(is.na(email)) {
+    email <- phone
+    phone <- NA
+  } else {
+    email <- email
+  }
   
   phd_adv <- scrape_text(page = page, selector = phd_selector)
   assertthat::assert_that(length(phd_adv) == 1 | is.null(phd_adv))
@@ -132,30 +168,32 @@ collect_info <- function(link, ischool,
   } else {
     specializations <- NULL
   }
-  
+
   if(length(research_area) > 0) {
-    research_area <- data.frame(email, research_area, stringsAsFactors = FALSE) 
+    research_area <- data.frame(email, research_area, stringsAsFactors = FALSE)
   } else {
     research_area <- NULL
   }
-  
+
   if(length(bio) > 0) {
-    bio <- data.frame(email, bio, stringsAsFactors = FALSE) 
+    bio <- data.frame(email, bio, stringsAsFactors = FALSE)
   } else {
     bio <- NULL
   }
-  
+
   if(length(edu) > 0 ) {
     edu <- data.frame(email, edu, stringsAsFactors = FALSE)
   } else {
     edu <- NULL
   }
-  
+
   if(length(pub) > 0) {
     pub <- data.frame(email, pub, stringsAsFactors = FALSE)
   } else {
     pub <- NULL
   }
+
+  Sys.sleep(delay)
 
   return(list(id = id, specializations = specializations, research_area = research_area,
               bio = bio, edu = edu, pub = pub))
@@ -184,7 +222,7 @@ faculty.uw <- function(url = "https://ischool.uw.edu/people/faculty") {
               pub_selector = ".pub > .title")
 
   l <- collapse(l)
-  
+
   return(l)
 }
 
@@ -223,5 +261,26 @@ faculty.um <- function(url = "https://www.si.umich.edu/people/directory/faculty?
 #' @title Scrape faculty page at University of Illinois
 faculty.illinois <- function(url = "https://ischool.illinois.edu/people/faculty") {
   
+  links <- scrape_links(url = url, selector = "div.profile-teaser__name > a")
   
+  links <- paste0("https://ischool.illinois.edu", gsub("/people/faculty", "", links))
+
+  l <- lapply(links, 
+              collect_info, 
+              ischool = "University of Illinois Urban Champagne",
+              name_selector = "#page-title > span",
+              title_selector = "body > div.dialog-off-canvas-main-canvas > main > div > div.basic-page__guttered-content > div.basic-page__main > div > article > div > div.profile__core-content > p.profile__title",
+              email_selector = "body > div.dialog-off-canvas-main-canvas > main > div > div.basic-page__guttered-content > div.basic-page__main > div > article > div > div.profile__core-content > div.profile__contact-information > p:nth-child(3)",
+              phone_selector = "body > div.dialog-off-canvas-main-canvas > main > div > div.basic-page__guttered-content > div.basic-page__main > div > article > div > div.profile__core-content > div.profile__contact-information > p:nth-child(2) > a",
+              phd_selector = NULL,
+              spec_selector = NULL,
+              research_selector = "body > div.dialog-off-canvas-main-canvas > main > div > div.basic-page__guttered-content > div.basic-page__main > div > article > div > div.profile__core-content > div.tag-list > ul > li > a",
+              bio_selector = "body > div.dialog-off-canvas-main-canvas > main > div > div.basic-page__guttered-content > div.basic-page__main > div > article > div > div.profile__core-content > div:nth-child(7) > div > p:nth-child(1)",
+              edu_selector = "body > div.dialog-off-canvas-main-canvas > main > div > div.basic-page__guttered-content > div.basic-page__main > div > article > div > div.profile__core-content > p.profile__education",
+              pub_selector = "body > div.dialog-off-canvas-main-canvas > main > div > div.basic-page__guttered-content > div.basic-page__main > div > article > div > div.profile__core-content > div:nth-child(10) > div > p")
+
+  l <- collapse(l)
+
+  return(l)
 }
+
